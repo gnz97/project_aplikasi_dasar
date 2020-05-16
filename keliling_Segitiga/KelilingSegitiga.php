836e778a07d04a8e601b1b09be1c0dbb118d5657
gnz97 <47927755+gnz97@users.noreply.github.com>
2020-05-17 02:22:07 +0700

    Update KelilingSegitiga.php

--- keliling_Segitiga/KelilingSegitiga.php
+++ keliling_Segitiga/KelilingSegitiga.php
@@ -6,37 +6,36 @@
 
 <body>
 
-<form method="POST" action="<?php $_SERVER['PHP_SELF']?>">
+        <form method="POST" action="<?php $_SERVER['PHP_SELF']?>">
 
-<input type="number" name="sisiA" placeholder="Sisi A">
-<br>
-<br>
-<input type="number" name="sisiB" placeholder="sisi B">
-<br>
-<br>
-<input type="number" name="sisiC" placeholder="sisi C">
-<br>
-<br>
-<input type="submit" name="hitung" value="Hitug">
+        <input type="number" name="sisiA" placeholder="Sisi A">
+        <br>
+        <br>
+        <input type="number" name="sisiB" placeholder="sisi B">
+        <br>
+        <br>
+        <input type="number" name="sisiC" placeholder="sisi C">
+        <br>
+        <br>
+        <input type="submit" name="hitung" value="Hitug">
 
-</form>
-<?php
+        </form>
+        <?php
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $sisi1 = $_POST['sisiA'];
-    $sisi2 = $_POST['sisiB'];
-    $sisi3 = $_POST['sisiC'];
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $sisi1 = $_POST['sisiA'];
+            $sisi2 = $_POST['sisiB'];
+            $sisi3 = $_POST['sisiC'];
 
-    if(!empty($_POST['hitung'])){
-        $k = $sisi1 + $sisi2 + $sisi3;
+            if(!empty($_POST['hitung'])){
+                $k = $sisi1 + $sisi2 + $sisi3;
 
-        echo "K = " .$k . " cm";
-    }
-}
+                echo "K = " .$k . " cm";
+            }
+        }
 
 
-?>    
-
+        ?>    
 
 </body>
 
